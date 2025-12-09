fbfa48190f7471fbde5bf7c94306478c85aea275
Maudeeeeeeeee <maudeboudineau@hotmail.fr>
2025-12-09 10:35:11 +0100

    version beta

--- src/player_mvt.py
+++ src/player_mvt.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 # Set up the display
-WIDTH, HEIGHT = 1280, 720
+from consts import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player:
     def __init__(self, x, y, size, speed):
@@ -26,19 +26,10 @@ class Player:
             self.y += self.speed
 
         # Keep cube inside the screen
-        self.x = max(0, min(self.x, WIDTH - self.size))
-        self.y = max(0, min(self.y, HEIGHT - self.size))
+        self.x = max(0, min(self.x, SCREEN_WIDTH - self.size))
+        self.y = max(0, min(self.y, SCREEN_HEIGHT - self.size))
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 255, 255), (self.x, self.y, self.size, self.size))
 
     
-# Create cube object in the center of the screenSS
-cube = Player(100, 100, 50, 5)
-cube.move(pygame.key.get_pressed())
-
-# Font for “0”
-font = pygame.font.SysFont(None, 40)
-
-# Draw "0" in top-left
-text = font.render("0", True, (255, 255, 255))
