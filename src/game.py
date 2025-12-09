1b8a02190d50cafcd687ed9ea383885fd0284657
Florian LAINE <florian.laine@epita.fr>
2025-12-09 18:19:34 +0900

    feat: add spawning enemies + move toward mouse

---
+++ src/game.py
@@ -0,0 +1,58 @@
+from typing import List
+import pygame
+from random import randint, randrange
+
+from consts import *
+from enemy import Enemy
+from object import GridObject
+
+
+class Game:
+
+    def __init__(self):
+        # Initialize Pygame
+        pygame.init()
+
+        # Set up the display
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.gridObjects: List[GridObject] = []
+
+    def run(self):
+        running = True
+
+        # Main game loop
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.fill("purple")
+
+            for obj in self.gridObjects:
+                obj.draw(self.screen)
+
+            pygame.display.flip()
+            self.clock.tick(60)
+            self.handleEvents()
+        pygame.quit()
+
+    def handleEvents(self):
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_SPACE]:
+            self.gridObjects.append(
+                Enemy(
+                    randint(0, SCREEN_WIDTH),
+                    randint(0, SCREEN_HEIGHT),
+                    CELL_SIZE,
+                    CELL_SIZE,
+                    randint(50, 200) / 100,
+                )
+            )
+
+        for obj in self.gridObjects:
+            obj.update()
+
+
+gameInstance = Game()
--- src/main.py
+++ src/main.py
@@ -1,21 +1,4 @@
-import pygame
+from game import gameInstance
 
-# Initialize Pygame
-pygame.init()
 
-# Set up the display
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-
-# Main game loop
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill("purple")
-    pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
+gameInstance.run()
